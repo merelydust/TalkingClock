74298a42175dde3c2133dc28d68f47c067977c43
river <namespaceshadow@gmail.com>
2023-10-22 17:11:39 +0200

    feat: add Antd & finish major parts of frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,30 @@
 import React, { useState, useEffect } from "react";
 import Clock from './components/Clock';
 import "./App.css";
+import { ClockCircleOutlined, PlayCircleOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+
+const items = [
+  getItem('Announce Time', 'sub1', <PlayCircleOutlined />, [
+    getItem('Audio Recording', 'g1', null, [getItem('English', 'en'), getItem('Chinese', 'ch'),], 'group'),
+    getItem('TTS Generator', 'g2', null, [getItem('English', 'tts-en'), getItem('Chinese', 'tts-ch'),], 'group'),
+  ]),
+  getItem('Set Alarm', 'sub2', <ClockCircleOutlined />, [
+    getItem('Alarm Clock', 'alarm'),
+    getItem('Countdown', 'countdown'),
+  ]),
+
+];
  
 function App() {
     // usestate for setting a javascript
@@ -29,13 +53,29 @@ function App() {
     //         })
     //     );
     // }, []);
+
+    const onClick = (e) => {
+        console.log('click ', e);
+      };
  
     return (
         <div className="App">
-            <div className="top"></div>
-            <div className="menu">This is a menu</div>
-            <Clock />
-            <div className="assistant"></div>
+            <Menu
+                onClick={onClick}
+                theme='dark'
+                style={{
+                    width: 256,
+                    height: '100vh',
+                }}
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                mode="inline"
+                items={items}
+            />
+            <div className="display">
+                <Clock />
+                <div className="assistant"></div>
+            </div>
         </div>
     );
 };
--- frontend/src/components/Clock.jsx
+++ frontend/src/components/Clock.jsx
@@ -1,9 +1,32 @@
 // src/Clock.jsx
 import React, { useState, useEffect } from 'react';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import { Button, Modal, TimePicker} from 'antd';
+import { PlayCircleOutlined } from '@ant-design/icons';
 import './Clock.css';
+dayjs.extend(customParseFormat);
 
 function Clock() {
   const [time, setTime] = useState(new Date());
+  const [myTime, setMyTime] = useState("00:00:00");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const clickTestBtn = () => {
+    showModal();
+  }
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   useEffect(() => {
     const timerID = setInterval(
@@ -16,13 +39,39 @@ function Clock() {
     };
   }, []);
 
-  function tick() {
+  const tick = () => {
     setTime(new Date());
+  }
+
+  const onChange = (time, timeString) => {
+    // console.log(time, timeString);
+    setMyTime(timeString);
+  };
+
+  const readMyTime = () => {
+    console.log(myTime);
   }
 
   return (
     <div className="clock-container">
       <h2>{time.toLocaleTimeString()}</h2>
+      <div className="test">
+
+      </div>
+      <Button
+        onClick={clickTestBtn}
+        style={{
+          fontWeight: 600,
+        }}
+      >Read My Time</Button>
+      <Modal title="Choose Time" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <TimePicker onChange={onChange} defaultValue={dayjs('00:00:00', 'HH:mm:ss')} size="large" />
+        <Button
+          icon={<PlayCircleOutlined />}
+          type="text" style={{marginLeft: 28}}
+          onClick={readMyTime}
+          >Play aloud</Button>
+      </Modal>
     </div>
   );
 }
